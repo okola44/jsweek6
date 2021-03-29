0eac2e3deca33cb33238b412a21059f92bf846b0
Okola44 <mercyimaliokola@gmail.com>
2021-03-29 08:43:40 +0300

    6th assignment

---
+++ assignment.js
@@ -0,0 +1,16 @@
+
+var kiosk={
+ get listOf(){
+    kiosk.getFRUITS =("mango");
+    
+ },
+ set listOf(value){
+    const FRUITS= value.listOf;
+   kiosk.setFRUITS=["mango"];
+kiosk.setFRUITS=["banana"];
+
+
+ }
+
+};
+console.log(kiosk);
